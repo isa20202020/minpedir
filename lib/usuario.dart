dcc2c36de379d9d730bbce66c202f17edaa963cd
Aluno 18 <isabellasantosd@811@gmail.com>
2025-08-25 14:47:26 -0300

    aplicando os conceiros de polimorfismo

--- lib/usuario.dart
+++ lib/usuario.dart
@@ -4,13 +4,25 @@ class Usuario{
   String? login ;
   String? senha ;
 
-  Usuario({this.codigo, this.senha, this.login, this.nome});
-
-  bool logar (String loginDigitado, String senhaDigitado) {
-    if (login == this.login && senha == this.senha) {
-      return true;
-    } else {
-      return false;
-    }
+  Usuario({int? codigo, String? senha, String? login, String? nome}){
+    _codigo = codigo;
+    _login = login;
+    _nome = nome;
+    _senha = senha;
   }
+  int? get codigo => _codigo;
+  String? get nome => _nome;
+  String? get senha => _senha;
+
+  String? get login{
+    return _login;
+  }
+
+  set nome(String? nome) => _nome = nome;
+  set senha(String? senha) => _senha = senha;
+  set codigo(int? codigo) => _codigo = codigo;
+  set login(String? login){
+    _login = login;
+  }
+
 }
